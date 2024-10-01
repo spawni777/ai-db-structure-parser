ee8c3f840102d2fea3bf6f956abaa367dbb27473
spawni777 <spawni777@gmail.com>
2024-10-01 15:39:54 +0300

    extract frontend port configuration to the .env file

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,11 +1,16 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
+
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: true,
-    port: 8000
+export default defineConfig(({mode}) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [react()],
+    server: {
+      host: true,
+      port: parseInt(env.VITE_PORT) || 8000, // default to port 3000 if not specified
+    }
   }
 })
